0cbcb30d851350b06a8f0e1066954756289fa60c
Ates Goral <ates@magnetiq.com>
2020-02-03 17:29:25 -0500

    Move middleware out

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import { Socket } from './lib/socket.js';
+import { cookieMiddleware } from './lib/middleware/cookieMiddleware.js';
+import { sessionMiddleware } from './lib/middleware/sessionMiddleware.js';
 
 // const clientSocket = new Socket();
 // const serverSocket = new Socket();
@@ -134,58 +136,6 @@ const bodyDecoders = {
   }
 }
 
-function decodeCookies(encoded) {
-  return encoded && encoded.split('; ');
-}
-
-async function cookieMiddleware(request) {
-  const cookies = decodeCookies(request.headers['Cookie']);
-  request.cookies = cookies;
-  return true;
-}
-
-const SESSION_COOKIE_NAME = '__SESSION';
-const SESSION_COOKIE_REGEX = new RegExp(`^${SESSION_COOKIE_NAME}=(.+)$`);
-const SESSION_ID_BITS = 31;
-const SESSION_ID_MIN = 2 ** (SESSION_ID_BITS - 1);
-const SESSION_ID_MAX = 2 ** SESSION_ID_BITS;
-
-const sessionDataMap = {};
-
-function getSessionId(request) {
-  const sessionCookie = request.cookies && request.cookies.find((cookie) => SESSION_COOKIE_REGEX.test(cookie));
-
-  if (sessionCookie) {
-    let [ _, sessionId ] = SESSION_COOKIE_REGEX.exec(sessionCookie);
-    return sessionId;
-  }
-}
-
-function randomRange(min, max) {
-  return Math.random() * (max - min) + min | 0;
-}
-
-function setNewSessionId(response) {
-  let sessionId = null;
-
-  do {
-    sessionId = `${randomRange(SESSION_ID_MIN, SESSION_ID_MAX).toString(16)}`;
-  } while (sessionDataMap[sessionId]);
-
-  response.headers['Set-Cookie'] = `${SESSION_COOKIE_NAME}=${sessionId}`;
-
-  return sessionId;
-}
-
-async function sessionMiddleware(request, response) {
-  const sessionId = getSessionId(request) || setNewSessionId(response);
-  const sessionData = sessionDataMap[sessionId] || (sessionDataMap[sessionId] = {});
-
-  request.session = sessionData;
-
-  return true;
-}
-
 async function bodyDecoderMiddleware(request) {
   const bodyDecoder = bodyDecoders[request.contentType];
 
@@ -241,6 +191,7 @@ const requestHandler = {
   }
 }
 
+// @todo rename to router middleware?
 function applicationMiddleware(_, response) {
   response.body = 'Hello World!';
   response.headers['Content-Type'] = 'text/plain';
