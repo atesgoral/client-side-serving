1f88d6f9667666d9af2e3390d0c785330262bfe2
Ates Goral <ates@magnetiq.com>
2020-02-05 21:47:19 -0500

    Stash old code

--- main.js
+++ main.js
@@ -11,8 +11,6 @@ import { HttpRequestFormatter } from './lib/httpRequestFormatter.js';
 import { HttpRequestParserStream } from './lib/httpRequestParserStream.js';
 import { HttpResponseFormatter } from './lib/httpResponseFormatter.js';
 
-const CRLF = '\r\n';
-
 const requestHandler = {
   firstMiddlewareNode: null,
   lastMiddlewareNode: null,
@@ -61,65 +59,6 @@ requestHandler.useMiddleware(sessionMiddleware);
 // requestHandler.useMiddleware(bodyDecoderMiddleware);
 requestHandler.useMiddleware(queryStringDecoderMiddleware);
 requestHandler.useMiddleware(applicationMiddleware);
-
-// const choppyTransport = {
-//   queue: [],
-//   timer: null,
-//   sendChunk(chunk) {
-//     this.queue.push(chunk);
-
-//     if (this.timer === null) {
-//       this.startTimer();
-//     }
-//   },
-//   startTimer() {
-//     this.timer = setTimeout(() => {
-//       const chunk = this.queue.shift();
-//       console.log('RX chunk', `(${chunk.length}) ${JSON.stringify(chunk)}`);
-//       this.rx(chunk);
-
-//       if (this.queue.length) {
-//         this.startTimer();
-//       } else {
-//         this.timer = null;
-//       }
-//     }, Math.random() * 500);
-//   },
-//   tx(data) {
-//     let remaining = data;
-//     let chunkLength = 0;
-
-//     do {
-//       chunkLength = Math.round(Math.random() * (remaining.length - 1)) + 1;
-//       this.sendChunk(remaining.substr(0, chunkLength));
-//       remaining = remaining.substr(chunkLength);
-//     } while (remaining.length);
-//   }
-// };
-
-// choppyTransport.rx = (data) => httpRequestReader.write(data);
-
-// const socket = {
-//   send(data) {
-//     choppyTransport.tx(data);
-//   }
-// };
-
-// socket.send(encodedRequest);
-
-// const raw = new ReadableStream({
-//   start(controller) {
-//     const encoder = new TextEncoder();
-//     controller.enqueue(encoder.encode(encodedRequest));
-//     controller.close();
-//   },
-//   pull(controller) {
-//     console.log('pull');
-//   },
-//   cancel() {
-//     console.log('cancel');
-//   }
-// });
 
 const httpRequestParser = new HttpRequestParser();
 
